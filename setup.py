640aa1a3a33153363492770a07d3656eeeb6582e
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-28 01:24:02 -0400

    Increment version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import dsargparse
 
 setup(
     name="dsargparse",
-    version="0.1.0",
+    version="0.1.1",
     author="Junpei Kawamoto",
     author_email="kawamoto.junpei@gmail.com",
     description=dsargparse.__doc__,
